da92503bd51fab8fe784a2cb131d39f588cb3327
The Byter <harrysaltzman@web.de>
2018-08-22 07:18:47 +0200

    Add files via upload

---
+++ GPIO/GPIO/Servo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using WiringPiLib;
+
+namespace GPIO1
+{
+	public class Servo
+	{
+		public float Position { get; private set;}
+		int Hertz{ get; set;}
+		WirinPiWrapper WiringPiLib{ get; set;}
+		uint PwmRange{ get; set;}
+		int PwmPin{ get; set;}
+		int MaximalAusschlagGrad{ get; set;}
+		int MicroSecLeft{ get; set;}
+		float MicroSecRange{ get; set;}
+		int PeriodeMicroSecs{ get; set;}
+
+		public Servo (int pwmPin, int hertz, int pwmRange, int maximalAusschlagGrad,  WirinPiWrapper wiringPiLib, int microSecLeft, int microSecRight)
+		{
+			WiringPiLib = wiringPiLib;
+
+			PwmRange = (uint)pwmRange;
+			//pwmFrequency in Hz = 19.2e6 Hz / pwmClock / pwmRange.
+			int PwmClock = 19200000 / pwmRange / hertz;
+
+			WiringPiLib.PinMode(pwmPin, PinType.PWM_OUTPUT) ;
+
+			WiringPiLib.PwmSetClock (PwmClock);
+			WiringPiLib.PwmSetMode (PwmType.PWM_MODE_MS);	//Servos likes mark / Space approach
+			WiringPiLib.PwmSetRange(PwmRange);
+
+			PwmPin = pwmPin;
+
+			MaximalAusschlagGrad = maximalAusschlagGrad;
+
+			MicroSecLeft = microSecLeft;
+
+			MicroSecRange = microSecRight - MicroSecLeft;
+
+			PeriodeMicroSecs = 1000000 / hertz;
+			Position = 1;
+			SetPosition (0);
+		}
+
+		public void SetPosition(float sollPosition)	//-90 bis + 90
+		{
+			if (sollPosition == Position)
+				return;
+
+			if (Math.Abs (sollPosition) > 90)
+				throw new ArgumentOutOfRangeException ("Maximalausschlag von 90 Grad kann nicht überschritten werden.");
+			
+			if (Math.Abs (sollPosition) > MaximalAusschlagGrad)
+				return;
+
+			Position = sollPosition;
+				
+			sollPosition = MaximalAusschlagGrad * Math.Sign (sollPosition);
+				
+			//Gängig ist ein 50-Hz-Signal (20 ms Periodenlänge), welches zwischen 500 Mikrosekunden (linker Anschlag, 0 Grad) und 2500 Mikrosekunden (rechter Anschlag, 180 Grad) auf High-Pegel und den Rest der Periodenlänge auf Low-Pegel ist.
+
+			float anteilVon180Grad = (sollPosition + 90) / 180;
+
+			float microSec = (MicroSecRange * anteilVon180Grad) + MicroSecLeft;
+
+			int pwmValue = (int) ((microSec * PwmRange) / PeriodeMicroSecs);
+
+			WiringPiLib.PwmWrite (PwmPin, pwmValue);
+		}
+	}
+}
+
---
+++ GPIO/GPIO/SuchHund.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using WiringPiLib;
+using System.Configuration;
+using System.Threading;
+using System.Diagnostics;
+using log4net;
+
+namespace GPIO1
+{
+	public class SuchHund
+	{
+		//Pinbelegung
+		const int PWMServoPin	= 18;	//parallel read won't work: pwm uses port 18 also!
+		readonly int[] Sensors = new int[] {23, 24, 25, 8, 7, 12, 16, 20 };
+		const int SHUTDOWN	=	21;
+		bool InvertSensorInput{ get; set; }	//true für weiße Linie auf schwarzem Boden
+		static int SleepBetweenActions;
+		Servo LenkServo { get; set;}
+
+		static ILog Log4Net = LogManager.GetLogger("SuchHund");
+
+		WirinPiWrapper WiringPiLib;
+
+		void Initializer()
+		{
+			WiringPiLib = new WirinPiWrapper();
+			WiringPiLib.WiringPiSetupGpio ();
+
+			WiringPiLib.PinMode(SHUTDOWN, PinType.INPUT) ;
+			WiringPiLib.PullUpDnControl (SHUTDOWN, PullUpType.PUD_UP);
+
+			for (int i = 0; i < Sensors.Length; i++) {
+				WiringPiLib.PinMode (Sensors [i], PinType.INPUT);
+				WiringPiLib.PullUpDnControl(Sensors[i], PullUpType.PUD_DOWN);
+			}
+
+			SleepBetweenActions = Int32.Parse(ConfigurationManager.AppSettings ["Hund_SleepBetweenActions"]);
+
+			int pwmRange = Int32.Parse(ConfigurationManager.AppSettings ["Hund_PWM_Range"]);
+			int servohertz = Int32.Parse(ConfigurationManager.AppSettings ["HundServohertz"]);
+			int servoMaximalAusschlagGrad = Int32.Parse(ConfigurationManager.AppSettings ["HundServoMaximalAusschlagGrad"]);
+			int hundServoMilliSecLeft = Int32.Parse(ConfigurationManager.AppSettings ["HundServoMicroSecLeft"]);
+			int hundServoMilliSecRight = Int32.Parse(ConfigurationManager.AppSettings ["HundServoMicroSecRight"]);
+			InvertSensorInput = bool.Parse(ConfigurationManager.AppSettings ["HundInvertSensorInput"]);
+
+			LenkServo = new Servo (PWMServoPin, servohertz, pwmRange, servoMaximalAusschlagGrad, WiringPiLib, hundServoMilliSecLeft, hundServoMilliSecRight);
+		}
+
+		public SuchHund ()
+		{
+			Initializer ();
+		}
+
+
+		//Definition 1 ist Linie, 0 ist Boden
+		private long ReadSensors ()
+		{
+			long result = 0;
+			
+			for (int i = 0; i < Sensors.Length; i++)
+				if (WiringPiLib.DigitalRead(Sensors[i]) > 0)
+					result = result | ((long)1 << i);
+
+			if (InvertSensorInput)
+				result = result ^ 0xffffffff;
+
+			WriteResult (result);
+
+			return result;
+		}
+
+		private bool SensorsPlausible(long sensorsResults)
+		{ 
+			//Sinnvoll ist es, wenn 
+			// 00001111 gerade, also Liniengrenze, Sollzustand
+			// 00000000 oder 11111111 : auf der Linie / ganz daneben
+			// 00011000  dünne linie ???
+			//Bloedsinn ist 
+			//11000011 Linie Boden Linie??
+			//01010101 Muster lesefehler
+			// Also: wenn ich von Links anfange, ist es sinnvoll, wenn hinter der ersten Linie 0..n Linien kommen und dann nur Boden
+			int i = 0;
+			while (((sensorsResults & ((long)1 << i)) == 0) && (i < Sensors.Length))
+				i++;
+				
+			while (((sensorsResults & ((long)1 << i)) > 0)&& (i < Sensors.Length))
+				i++;
+
+			while (((sensorsResults & ((long)1 << i)) == 0)&& (i < Sensors.Length))
+				i++;
+
+			if (i >= Sensors.Length) 
+			{
+				Log4Net.Info ("Plausibel");
+				return true;
+			}
+
+			Log4Net.Info ("NICHT plausibel");
+			return false;
+		}
+
+
+		enum Direction
+		{
+			Left = -1,
+			Straight = 0,
+			Right = 1
+		}
+
+		private Direction GetSollDirection(long sensors)
+		{
+			int i = 0;
+			while (((sensors & ((long)1 << i)) == 0) && (i < Sensors.Length))
+				i++;
+
+			while (((sensors & ((long)1 << i)) == 1) && (i < Sensors.Length))
+				i++;
+
+			//i steht jetzt am Linken Rand der Linie
+			if (i < Sensors.Length / 2)
+				return Direction.Left;
+
+			if (i == Sensors.Length / 2)
+				return Direction.Straight;
+
+			return Direction.Right;
+		}
+
+		private void Lenke(Direction direction)
+		{
+			Log4Net.Info ($"Lenke: {direction}, Alte Position: {LenkServo.Position}");
+
+			if (direction == Direction.Straight) 
+			{
+				LenkServo.SetPosition (0);
+			}
+			
+			if (direction == Direction.Left) 
+			{
+				if(LenkServo.Position > -90)
+					LenkServo.SetPosition (LenkServo.Position - 1);
+			}
+
+			if (direction == Direction.Right) 
+			{
+				if(LenkServo.Position < 90)
+					LenkServo.SetPosition (LenkServo.Position + 1);
+			}
+		}
+
+		public void SensorTest()
+		{
+			while (true) 
+			{
+				long sensorResult = ReadSensors ();
+
+				if(SensorsPlausible (sensorResult))
+					Log4Net.Info (GetSollDirection (sensorResult));
+
+				Thread.Sleep (1000);
+			}
+		}
+
+		private void WriteResult(long result)
+		{
+			string output = "Result: ";
+
+			for (int i = 0; i < Sensors.Length; i++)
+				if ((result & (1 << i)) > 0)
+					output += "1";
+				else
+					output += "0";
+
+			Log4Net.Info (output);
+		}
+
+
+		public void ServoTest()
+		{
+			while (true) 
+			{
+				for(int i = -90; i < 90; i++)
+				{
+					LenkServo.SetPosition(i);
+					Thread.Sleep (5);
+				}
+				for(int i = 90; i > -90; i--)
+				{
+					LenkServo.SetPosition(i);
+					Thread.Sleep (5);
+				}
+			}
+		}
+
+		public void Suche()
+		{
+			while (true) 
+			{
+				if (WiringPiLib.DigitalRead (SHUTDOWN) == 0)
+					Process.Start ("/usr/bin/sudo", "/sbin/shutdown -h now");
+
+				long sensorResult = ReadSensors ();
+
+				if (SensorsPlausible (sensorResult)) 
+				{
+					Lenke (GetSollDirection (sensorResult));
+					Thread.Sleep (SleepBetweenActions);
+				}
+			}
+		}
+	}
+}
